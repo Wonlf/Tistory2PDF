ae11ab90d7f862bccaf7e3a6102c0709350379e1
chosungwon <qqwow741@gmail.com>
2022-04-09 23:04:36 +0900

    Implementation completed to leave only desired elements in HTML

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,11 +25,45 @@ public class Main {
 
         try {
             Document document = conn.get();
-            Elements imageUrlElements = document.getElementsByClass("e-content post-content");
+            Elements cssElement = document.getElementsByTag("head");
 
-            FileWriter fileWriter = new FileWriter(filePath, true);
-            fileWriter.write(imageUrlElements.toString());
 
+            Element imageUrlElements = document.getElementById("container");
+
+            Elements sidebarElement =  imageUrlElements.select("#sidebar"); //사이드바
+            Elements rmElement1 =  imageUrlElements.select("#lightbox");
+            Elements rmElement2 =  imageUrlElements.select("#lightboxOverlay");
+            Elements rmElement3 =  imageUrlElements.select(".entry-tag"); //블로그 글 태그
+            Elements rmElement4 =  imageUrlElements.select(".container_postbtn"); //태그
+            Elements rmElement5 =  imageUrlElements.select(".another_category"); //다른글
+            //Elements rmElement6 =  imageUrlElements.select(".sns-go"); //sns 아래를 띄워줘서 굳이 지울 필요 없을 듯
+            Elements rmElement7 =  imageUrlElements.select(".area_related_wrap"); //관련글
+            Elements rmElement8 =  imageUrlElements.select(".post-reply"); //댓글
+            Elements rmElement9 =  imageUrlElements.select("#paging"); //페이징
+            Elements rmElement10 =  cssElement.select("script");
+            sidebarElement.remove();
+            rmElement1.remove();
+            rmElement2.remove();
+            rmElement3.remove();
+            rmElement4.remove();
+            rmElement5.remove();
+            //rmElement6.remove();
+            rmElement7.remove();
+            rmElement8.remove();
+            rmElement9.remove();
+            rmElement10.remove();
+
+
+
+            FileWriter fileWriter = new FileWriter(filePath); //2번째 인자로 true 넣으면 append옵션 활성화
+
+            imageUrlElements.prepend("<link rel=\"stylesheet\" href=\"./content.css\">");
+            imageUrlElements.prepend("<link rel=\"stylesheet\" href=\"./main.css\">");
+
+            String html = imageUrlElements.toString();
+
+
+            fileWriter.write(html);
             fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
